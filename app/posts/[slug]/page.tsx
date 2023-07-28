50474e7055fcc8c54d970fb2cac0138fc9a14f42
alexRicc2 <alex.65.santanna@gmail.com>
2023-07-28 06:24:04 -0300

    first

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import { getPostBySlug, getPostTest } from "../../../lib/api";
+import markdownToHtml from "../../../lib/markdownToHtml";
+import markdownStyles from "./markdown-styles.module.css";
+
+export default async function Post({ params }: { params: { slug: string } }) {
+  // const post = getPostBySlug(params.slug, ["title", "author", "content"]);
+  const data = await getPostTest(params?.slug);
+
+  // const content = await markdownToHtml(post.content || "");
+
+  return (
+    <div className="container mx-auto">
+      <main>
+        <div className="w-full h-16  text-white">
+          <p className="text-5xl">{data?.post?.title}</p>
+          {/* <p className="text-2xl">{post?.title}</p> */}
+          {/* <p className="text-gray-400">{post?.author}</p> */}
+          {/* <div */}
+          {/* className={markdownStyles["markdown"]} */}
+          {/* dangerouslySetInnerHTML={{ __html: content }} */}
+          {/* /> */}
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ lib/api.ts
@@ -0,0 +1,99 @@
+import fs from "fs";
+import { join } from "path";
+import matter from "gray-matter";
+import { isEmpty} from 'lodash';
+const API_URL =
+  `${process.env.WORDPRESS_API_URL}/graphql` ;
+export async function fetchAPI(
+  query = "",
+  { variables }: Record<string, any> = {},
+  authToken = ""
+) {
+  const headers: HeadersInit = { "Content-Type": "application/json" };
+
+  //if has authToken it will add the token to the post header, used to get post previews for example
+  if (!isEmpty(authToken)) {
+    headers["Authorization"] = `Bearer ${authToken}`;
+  }
+  console.log('bodyyy pro postman',{
+    query, variables
+  })
+  //WPGraphql Plugin must be enabled
+  const res = await fetch(API_URL, {
+    headers,
+    method: 'POST',
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  })
+
+  const json = await res.json()
+  if (json.errors) {
+    console.error(json.errors)
+    throw new Error('Failed to fetch API')
+  }
+  return json.data
+  
+}
+const postsDirectory = join(process.cwd(), "_posts");
+
+export function getPostSlugs() {
+  return fs.readdirSync(postsDirectory);
+}
+
+export async function getPostTest(slug: string | undefined | string[]){
+  const data = await fetchAPI(`query PostBySlug($id: ID!, $idType: PostIdType!){
+    post(id: $id, idType: $idType){
+      title
+    } 
+  }`,
+  {
+    variables: {
+      id: slug,
+      idType: "SLUG",
+    },
+  })
+
+  console.log('data na api back', data)
+  return data
+}
+
+
+export function getPostBySlug(slug: string, fields: string[] = []) {
+  const realSlug = slug.replace(/\.md$/, "");
+  const fullPath = join(postsDirectory, `${realSlug}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+  const { data, content } = matter(fileContents);
+
+  type Items = {
+    [key: string]: string;
+  };
+
+  const items: Items = {};
+
+  // Ensure only the minimal needed data is exposed
+  fields.forEach((field) => {
+    if (field === "slug") {
+      items[field] = realSlug;
+    }
+    if (field === "content") {
+      items[field] = content;
+    }
+
+    if (typeof data[field] !== "undefined") {
+      items[field] = data[field];
+    }
+  });
+
+  return items;
+}
+
+export function getAllPosts(fields: string[] = []) {
+  const slugs = getPostSlugs();
+  const posts = slugs
+    .map((slug) => getPostBySlug(slug, fields))
+    // sort posts by date in descending order
+    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+  return posts;
+}
